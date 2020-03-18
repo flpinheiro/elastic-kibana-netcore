d141acf155deaa0ed4502eb1a33b8979c2addc80
Felipe Pinheiro <flpinheiro@gmail.com>
2020-03-18 17:28:26 -0300

    project 2

--- ElasticKibanaNetCore2/Startup.cs
+++ ElasticKibanaNetCore2/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,7 +12,7 @@ using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Sinks.Elasticsearch;
 
-namespace ElasticKibanaNetCore
+namespace ElasticKibanaNetCore2
 {
     public class Startup
     {
@@ -35,12 +36,12 @@ namespace ElasticKibanaNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app,
-            IWebHostEnvironment env,
+        public void Configure(IApplicationBuilder app, 
+            IWebHostEnvironment env, 
             ILoggerFactory loggerFactory)
         {
             if (env.IsDevelopment())
@@ -49,11 +50,14 @@ namespace ElasticKibanaNetCore
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
             loggerFactory.AddSerilog();
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
@@ -62,7 +66,9 @@ namespace ElasticKibanaNetCore
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
